1a7caffbdcd4911dceabb12c77f19b0e823fa7e8
L.V.B. <100384938+LeoViBo@users.noreply.github.com>
2025-09-19 06:43:17 -0300

    Ajustes de erros

--- src/main/java/com/NextTech/SOASprint/controller/PerfilController.java
+++ src/main/java/com/NextTech/SOASprint/controller/PerfilController.java
@@ -1,7 +1,9 @@
 package com.NextTech.SOASprint.controller;
 
+import java.awt.print.Pageable;
 import java.net.URI;
 
+import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,8 +14,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.NextTech.SOASprint.dto.PerfilDTOs.PerfilResponseDTO;
+import com.NextTech.SOASprint.dto.PerfilDTOs.PerfilUpdateDTO;
+import com.NextTech.SOASprint.dto.PerfilDTOs.PerfilCreateDTO;
 import com.NextTech.SOASprint.service.PerfilService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
@@ -24,7 +31,7 @@ public class PerfilController {
     private final PerfilService service;
 
     @Operation(summary = "Cria um novo perfil de investimento")
-    @ApiResponse(responseCode = "201", description = "Perfil criado")
+    @ApiResponses(responseCode = "201", description = "Perfil criado")
     @PostMapping
     public ResponseEntity<Void> criar(@Valid @RequestBody PerfilCreateDTO dto) {
         Long id = service.criar(dto);
